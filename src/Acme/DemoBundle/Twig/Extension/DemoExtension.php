f9126f05b3d34e12fe5b2ffd323a531afdce0036
Thomas Ploch <t.ploch@reizwerk.com>
2013-02-06 16:23:46 +0100

    Merged 2.2 branch from standards edition

--- src/Acme/DemoBundle/Twig/Extension/DemoExtension.php
+++ src/Acme/DemoBundle/Twig/Extension/DemoExtension.php
@@ -2,8 +2,6 @@
 
 namespace Acme\DemoBundle\Twig\Extension;
 
-use Symfony\Component\HttpKernel\KernelInterface;
-use Symfony\Bundle\TwigBundle\Loader\FilesystemLoader;
 use CG\Core\ClassUtils;
 
 class DemoExtension extends \Twig_Extension
@@ -11,7 +9,7 @@ class DemoExtension extends \Twig_Extension
     protected $loader;
     protected $controller;
 
-    public function __construct(FilesystemLoader $loader)
+    public function __construct(\Twig_LoaderInterface $loader)
     {
         $this->loader = $loader;
     }
@@ -33,7 +31,10 @@ class DemoExtension extends \Twig_Extension
 
     public function getCode($template)
     {
-        $controller = htmlspecialchars($this->getControllerCode(), ENT_QUOTES, 'UTF-8');
+        // highlight_string highlights php code only if '<?php' tag is present.
+        $controller = highlight_string("<?php" . $this->getControllerCode(), true);
+        $controller = str_replace('<span style="color: #0000BB">&lt;?php&nbsp;&nbsp;&nbsp;&nbsp;</span>', '&nbsp;&nbsp;&nbsp;&nbsp;', $controller);
+
         $template = htmlspecialchars($this->getTemplateCode($template), ENT_QUOTES, 'UTF-8');
 
         // remove the code block
